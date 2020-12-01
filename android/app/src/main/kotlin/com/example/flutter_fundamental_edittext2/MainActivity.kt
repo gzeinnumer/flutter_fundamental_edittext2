d44020bfd6553516a8b9c98e7cbf7ea6038e6f2d
M. Fadli Zein <45892408+gzeinnumer@users.noreply.github.com>
2020-12-01 23:09:11 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/flutter_fundamental_edittext2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_fundamental_edittext2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Latihan TextField Style"),
+        ),
+        body: Container(
+          margin: EdgeInsets.all(30),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              MyTextField1(),
+              MyTextField2(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  TextField MyTextField1() {
+    return TextField(
+      maxLength: 20,
+      decoration: InputDecoration(
+        fillColor: Colors.black12,
+        filled: true,
+        icon: Icon(Icons.adb),
+        prefix: Container(
+          width: 5,
+          height: 5,
+          color: Colors.red,
+        ),
+        //1
+        prefixIcon: Icon(
+          Icons.person,
+          color: Colors.red,
+        ),
+        prefixStyle: TextStyle(
+          color: Colors.blueAccent,
+          fontWeight: FontWeight.w600,
+        ),
+        labelText: "labelText",
+        labelStyle: TextStyle(
+          color: Colors.blueAccent,
+        ),
+        hintText: "hintText",
+        hintStyle: TextStyle(
+          fontSize: 12,
+        ),
+        suffix: Container(
+          width: 5,
+          height: 5,
+          color: Colors.red,
+        ),
+        //1
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+      ),
+    );
+  }
+
+  TextField MyTextField2() {
+    return TextField(
+      maxLength: 20,
+      decoration: InputDecoration(
+        fillColor: Colors.black12,
+        filled: true,
+        icon: Icon(Icons.adb),
+        prefixText: "prefixText : ",
+        //2 //pilih salah satu
+        prefixIcon: Icon(
+          Icons.person,
+          color: Colors.red,
+        ),
+        prefixStyle: TextStyle(
+          color: Colors.blueAccent,
+          fontWeight: FontWeight.w600,
+        ),
+        labelText: "labelText",
+        labelStyle: TextStyle(
+          color: Colors.blueAccent,
+        ),
+        hintText: "hintText",
+        hintStyle: TextStyle(
+          fontSize: 12,
+        ),
+        suffixText: "suffixText : ",
+        //2 //pilih salah satu
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+      ),
+    );
+  }
+}
